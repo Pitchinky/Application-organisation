c231164783817e540f60a5e367a6bcff46db884e
camille-aubry <127899262+camille-aubry@users.noreply.github.com>
2026-03-02 10:35:30 +0100

    modif app

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 const WEATHER_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 // Remplace l'ancienne ligne par celle-ci (on ajoute .readonly)
-const SCOPES = "https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.readonly";
+const SCOPES = "https://www.googleapis.com/auth/calendar.events";
 const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
 
 function App() {
